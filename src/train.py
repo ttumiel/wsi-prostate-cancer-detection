299adab32f1808830b51bde9405119c9d1ed2bcf
Thomas Tumiel <ttumiel@gmail.com>
2020-06-25 21:21:30 +0200

    add ordinal regression and clean up model steps

--- src/train.py
+++ src/train.py
@@ -17,7 +17,7 @@ from table_logger import TableLogger
 
 class BaselineModel(pl.LightningModule):
     def __init__(self, path, lr=1e-3, bs=128, imsize=128, reg=True,
-                n_patches=9, is_stack=False):
+                n_patches=9, is_stack=False, epochs=None, norm_output=True, ordinal=False):
         super().__init__()
         self.path = path
         self.lr = lr
@@ -26,15 +26,23 @@ class BaselineModel(pl.LightningModule):
         self.reg = reg
         self.n_patches = n_patches
         self.is_stack = is_stack
+        self.epochs = epochs
+        self.norm_output = norm_output
+        self.ordinal = ordinal
 
         if reg:
             if is_stack:
-                self.model = EffNetPatch(n=1, n_patches=n_patches)
+                self.model = EffNetPatch(n=5 if ordinal else 1, n_patches=n_patches)
             else:
-                self.model = EffNetConcat(n=1, n_patches=n_patches)
-            # self.loss_fn = F.mse_loss
-            self.loss_fn = nn.SmoothL1Loss()
-            # self.loss_fn = nn.L1Loss()
+                self.model = EffNetConcat(n=5 if ordinal else 1, n_patches=n_patches)
+
+            if ordinal:
+                self.loss_fn = nn.BCEWithLogitsLoss()
+                # Try Focal Loss
+            else:
+                # self.loss_fn = F.mse_loss
+                self.loss_fn = nn.SmoothL1Loss()
+                # self.loss_fn = nn.L1Loss()
         else:
             self.model = EfficientNet.from_pretrained("efficientnet-b0", num_classes=6)
             # self.loss_fn = LabelSmoothingLoss(6, 0.1)
@@ -44,46 +52,61 @@ class BaselineModel(pl.LightningModule):
         self.hparams = {'lr': lr, 'bs':bs, 'patch_size':imsize,
                         'n_patches': n_patches, 'loss_fn': self.loss_fn.__class__.__name__,
                         'tfms': str(get_transforms(imsize, train=True, local=True, is_stack=is_stack)),
-                        'is_stack': is_stack}
+                        'is_stack': is_stack, 'path': str(self.path), 'ordinal': ordinal, 'norm_target': norm_output}
 
     def forward(self, x):
         return self.model(x)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
+        y_hat = self(x)
+
         if self.reg:
-            y_hat = self(x).squeeze(1)
-            loss = self.loss_fn(y_hat, y)
-            preds_amax = y_hat.add(2.5).round_()
-            y = y.add(2.5)
+            y_hat = y_hat.squeeze(1)
+
+        loss = self.loss_fn(y_hat, y)
+        y_hat = y_hat.detach()
+
+        if self.reg:
+            if self.norm_output:
+                y_hat = y_hat.add(2.5)
+                y = y.add(2.5)
+            elif self.ordinal:
+                y_hat = y_hat.sigmoid().sum(1)
+                y = y.sum(1)
+            preds = y_hat.round()
         else:
-            y_hat = self(x)
-            loss = self.loss_fn(y_hat, y)
-            preds_amax = y_hat.argmax(1)
+            preds = y_hat.argmax(1)
 
         tensorboard_logs = {
             'train/lr': self.trainer.optimizers[0].param_groups[0]['lr'],
-            'train/acc': accuracy(preds_amax, y),
+            'train/acc': accuracy(preds, y),
             'train/loss': loss,
-            'train/qk': quadratic_kappa(preds_amax, y)
+            'train/qk': quadratic_kappa(preds, y)
         }
         return {'loss': loss, 'log': tensorboard_logs}
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
+        y_hat = self(x).detach()
+
         if self.reg:
-            y_hat = self(x).squeeze(1)
-            preds_amax = y_hat.add(2.5).round()
-            loss = self.loss_fn(y_hat, y).detach()
-            y = y.add(2.5)
-            acc = accuracy(preds_amax, y)
+            y_hat = y_hat.squeeze(1)
+
+        loss = self.loss_fn(y_hat, y).detach()
+
+        if self.reg:
+            if self.norm_output:
+                y_hat = y_hat.add(2.5)
+                y = y.add(2.5)
+            elif self.ordinal:
+                y_hat = y_hat.sigmoid().sum(1)
+                y = y.sum(1)
+            preds = y_hat.round_()
         else:
-            y_hat = self(x)
-            preds_amax = y_hat.argmax(1)
-            acc = accuracy(preds_amax, y)
-            loss = self.loss_fn(y_hat, y).detach()
-        qk = quadratic_kappa(preds_amax, y)
-        return {'val_loss': loss, 'acc': acc, 'qk': qk}
+            preds = y_hat.argmax(1)
+
+        return {'val_loss': loss, 'acc': accuracy(preds, y), 'qk': quadratic_kappa(preds, y)}
 
     def validation_epoch_end(self, outputs):
         avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
@@ -95,16 +118,19 @@ class BaselineModel(pl.LightningModule):
     def configure_optimizers(self):
         optim = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=3e-6)
         sched = torch.optim.lr_scheduler.ReduceLROnPlateau(optim, mode='min', factor=0.5, patience=2, cooldown=1, min_lr=1e-6)
+        if self.epochs>10: sched = torch.optim.lr_scheduler.CosineAnnealingLR(optim, self.epochs)
         return [optim], [sched]
 
     def train_dataloader(self):
         DS = PandaDatasetStack if self.is_stack else PandaDatasetConcat
-        return DataLoader(DS(self.path, patch_size=self.imsize, n_patches=self.n_patches),
+        return DataLoader(DS(self.path, patch_size=self.imsize, n_patches=self.n_patches,
+                            ordinal=self.ordinal, norm_target=self.norm_output),
                           batch_size=self.bs, num_workers=7, shuffle=True, pin_memory=True)
 
     def val_dataloader(self):
         DS = PandaDatasetStack if self.is_stack else PandaDatasetConcat
-        return DataLoader(DS(self.path, patch_size=self.imsize, n_patches=self.n_patches, train=False),
+        return DataLoader(DS(self.path, patch_size=self.imsize, n_patches=self.n_patches,
+                            train=False, ordinal=self.ordinal, norm_target=self.norm_output),
                           batch_size=self.bs*4, num_workers=7, pin_memory=True)
 
 
@@ -120,7 +146,8 @@ def set_grad(m, grad):
 if __name__ == '__main__':
     path = Path('/path/to/data')
 
-    model = BaselineModel(path, bs=14, lr=3e-4, imsize=128, n_patches=64, reg=True, is_stack=False)
+    model = BaselineModel(path, bs=14, lr=3e-4, imsize=128, n_patches=64,
+                        reg=True, is_stack=False, epochs=5, norm_output=False, ordinal=True)
     # model = BaselineModel.load_from_checkpoint('./TableLogger/version_15/epoch=13.ckpt', map_location='cuda:0',
     #                                           path=path, bs=14, imsize=200, lr=1e-3, n_patches=22, reg=True, is_stack=False)
 
@@ -144,8 +171,9 @@ if __name__ == '__main__':
 
     # Train full network
     model.model.m.apply(partial(set_grad, grad=True))
-    model.lr = model.lr/5.
-    trainer = pl.Trainer(max_epochs=50, logger=logger, accumulate_grad_batches=1,
+    model.epochs = 30
+    # model.lr = model.lr/5.
+    trainer = pl.Trainer(max_epochs=30, logger=logger, accumulate_grad_batches=1,
                         checkpoint_callback=checkpoint_callback,
                         train_percent_check=1.0, val_percent_check=1.0,
                         gpus=1, weights_summary='top', precision=16)
